5a8f62f5f41165ecf5e561a81c6201a07d14a20b
hawken <askhaokun@gmail.com>
2023-04-27 17:04:11 +0800

    Update getwords.ts

--- pages/api/getwords.ts
+++ pages/api/getwords.ts
@@ -10,7 +10,7 @@ const openai = new OpenAIApi(configuration);
 // Initializing the cors middleware
 // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
 const cors = Cors({
-    origin: ["https://hawken-im.github.io/"],//TODO: add authentication method later, so I can change it to true.
+    origin: ["https://hawken-im.github.io/*"],//TODO: add authentication method later, so I can change it to true.
     //origin: true,
     methods: ['POST', 'GET', 'HEAD'],
 })
