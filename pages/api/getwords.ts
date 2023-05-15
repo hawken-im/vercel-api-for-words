44a999281754b43d4393eb0d3889febc71b18823
hawken <askhaokun@gmail.com>
2023-05-15 14:46:49 +0800

    Update getwords.ts

--- pages/api/getwords.ts
+++ pages/api/getwords.ts
@@ -89,6 +89,6 @@ export default async function handler(
 }
   
 function generatePrompt(text, vocabulary) {
-  return `As an English language expert, your task is to analyze the word list in CSV format provided within triple quotes and identify words that might be unfamiliar to a non-native English speaker who is familiar with the ${vocabulary} most common English words, or a vocabulary level of ${vocabulary}. Keep in mind that a native English speaker has an average vocabulary of 15,000 words. Return a list of the unfamiliar words in CSV format, only the words you picked out, nothing else, for example: "word1,word2,word3". If you believe that they know all the words, return: "none". Here is the words list:
+  return `As an English language expert, your task is to analyze the words list in CSV format provided within triple quotes and identify words that might be unfamiliar to a non-native English speaker who is familiar with the ${vocabulary} most common English words, or a vocabulary level of ${vocabulary}. Keep in mind that a native English speaker has an average vocabulary of 15,000 words. Return a list of the unfamiliar words in CSV format, only the words you picked out, nothing else, for example: "word1,word2,word3". If you believe that they know all the words, return: "none". Here is the words list:
   """${text}"""`;
 }
