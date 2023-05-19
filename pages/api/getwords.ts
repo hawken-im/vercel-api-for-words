dc2d5a5a370e88e68c59d6b52f0b7700aa602ab7
hawken <askhaokun@gmail.com>
2023-05-19 22:10:13 +0800

    Update getwords.ts

--- pages/api/getwords.ts
+++ pages/api/getwords.ts
@@ -122,15 +122,29 @@ export default async function handler(
 //   """${text}"""`;
 // }
 
+// function generatePrompt(text, vocabulary) {
+//   return `As an English language expert, your task is to analyze the given words list in CSV format and identify words that might be unfamiliar to a non-native English speaker who is familiar with an amount of most common English words, or vocabulary level. Keep in mind that a native English speaker has an average vocabulary of 15,000 words, but a non-native English speaker with IELTS test score of 6 has an average vocabulary of 5,000 words. Considering the given vocabulary level, return a list of the unfamiliar words in CSV format. If you really sure that the non-native English speaker knows all the words, return: "none".
+//   <Given words list>flood,pencil,skirt,portmanteau,stall,carve,ledge,respite,reproach,offal,rancid,midriff,chivvy,tureen,inveigle,ceiling,meadow,stance,shovel,cute,plank
+//   <Vocabulary level>4000
+//   <Unfamiliar words list>portmanteau,stall,respite,reproach,offal,rancid,midriff,chivvy,tureen,inveigle,plank
+//   <Given words list>hollow,flood,pencil,forbid,stern,ingratiate,plank,eavesdrop,skirt,legerdemain,cute,hamper,gleam,shrill,tickle,uxoricide,accomplish,shaggy,drip,warranty
+//   <Vocabulary level>8000
+//   <Unfamiliar words list>ingratiate,eavesdrop,legerdemain,uxoricide,hamper
+//   <Given words list>${text}
+//   <Vocabulary level>${vocabulary}
+//   <Unfamiliar words list>`;
+// }
+
 function generatePrompt(text, vocabulary) {
   return `As an English language expert, your task is to analyze the given words list in CSV format and identify words that might be unfamiliar to a non-native English speaker who is familiar with an amount of most common English words, or vocabulary level. Keep in mind that a native English speaker has an average vocabulary of 15,000 words, but a non-native English speaker with IELTS test score of 6 has an average vocabulary of 5,000 words. Considering the given vocabulary level, return a list of the unfamiliar words in CSV format. If you really sure that the non-native English speaker knows all the words, return: "none".
-  <Given words list>flood,pencil,skirt,portmanteau,stall,carve,ledge,respite,reproach,offal,rancid,midriff,chivvy,tureen,inveigle,ceiling,meadow,stance,shovel,cute,plank
+  <Given words list>flood,pencil,skirt,stall,carve,ledge,ceiling,meadow,stance,shovel,cute,plank,next,pay,enough,best,flow,clever,shake,sham,rind,weasel,grouse,bury,shrivel,pastiche,discomfit,sully
   <Vocabulary level>4000
-  <Unfamiliar words list>portmanteau,stall,respite,reproach,offal,rancid,midriff,chivvy,tureen,inveigle,plank
-  <Given words list>hollow,flood,pencil,forbid,stern,ingratiate,plank,eavesdrop,skirt,legerdemain,cute,hamper,gleam,shrill,tickle,uxoricide,accomplish,shaggy,drip,warranty
+  <Unfamiliar words list>stall,ledge,ceiling,stance,sham,rind,weasel,grouse,shrivel,pastiche,discomfit
+  <Given words list>flood,pencil,skirt,stall,carve,ledge,ceiling,meadow,stance,shovel,cute,plank,next,pay,enough,best,flow,clever,shake,sham,rind,weasel,grouse,bury,shrivel,pastiche,discomfit,sully
   <Vocabulary level>8000
-  <Unfamiliar words list>ingratiate,eavesdrop,legerdemain,uxoricide,hamper
+  <Unfamiliar words list>rind,grouse,shrivel,pastiche,discomfit
   <Given words list>${text}
   <Vocabulary level>${vocabulary}
   <Unfamiliar words list>`;
 }
+
