e161e7a5a373d3527106736f6711820810b859ae
hawken <askhaokun@gmail.com>
2023-06-19 10:43:19 +0800

    Update getwords.ts

--- pages/api/getwords.ts
+++ pages/api/getwords.ts
@@ -72,14 +72,6 @@ export default async function handler(
     }
   
     try {
-      // const completion = await openai.createCompletion({
-      //   model: "text-davinci-003",
-      //   prompt: generatePrompt(text,vocabulary),
-      //   max_tokens: 500,
-      //   temperature: 0.2,
-      //   n: 1,
-      // });
-      // const parsedResult = completion.data.choices[0].text.trim();
       const completion = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
         messages: generatePrompt(text,vocabulary),
@@ -103,49 +95,6 @@ export default async function handler(
       }
     }
 }
-  
-// function generatePrompt(text, vocabulary, maxreturn) {
-//   return `As an English language expert, your task is to analyze the words list in CSV format provided within triple quotes and identify words that might be unfamiliar to a non-native English speaker who is familiar with the ${vocabulary} most common English words, or a vocabulary level of ${vocabulary}. Keep in mind that a native English speaker has an average vocabulary of 15,000 words, but a non-native English speaker with IELTS test score of 6 has an average vocabulary of 6,000 words which is almost the same as a 8-year-old child. Considering the given vocabulary level (${vocabulary} words), it is important to only return a list of the ${maxreturn} most unfamiliar words in CSV format, no more than ${maxreturn} words. Include only the words you picked out, nothing else, for example: "word1,word2,word3". If you really sure that the non-native English speaker know all the words, return: "none". Here is the words list:
-//   """${text}"""`;
-// }
-
-// function generatePrompt(text, vocabulary) {
-//   return `As an English language expert, your task is to analyze the given words list in CSV format and identify words that might be unfamiliar to a non-native English speaker who is familiar with an amount of most common English words, or vocabulary level. Keep in mind that a native English speaker has an average vocabulary of 15,000 words, but a non-native English speaker with IELTS test score of 6 has an average vocabulary of 5,000 words. Considering the given vocabulary level, return a list of the unfamiliar words in CSV format. If you really sure that the non-native English speaker knows all the words, return: "none".
-//   <Given words list>flood,pencil,skirt,portmanteau,stall,carve,ledge,respite,reproach,offal,rancid,midriff,chivvy,tureen,inveigle,ceiling,meadow,stance,shovel,cute,plank
-//   <Vocabulary level>4000
-//   <Unfamiliar words list>portmanteau,stall,respite,reproach,offal,rancid,midriff,chivvy,tureen,inveigle,plank
-//   <Given words list>hollow,flood,pencil,forbid,stern,ingratiate,plank,eavesdrop,skirt,legerdemain,cute,hamper,gleam,shrill,tickle,uxoricide,accomplish,shaggy,drip,warranty
-//   <Vocabulary level>8000
-//   <Unfamiliar words list>ingratiate,eavesdrop,legerdemain,uxoricide,hamper
-//   <Given words list>${text}
-//   <Vocabulary level>${vocabulary}
-//   <Unfamiliar words list>`;
-// }
-
-// function generatePrompt(text, vocabulary) {
-//   return `As an English language expert, your task is to analyze the given words list in CSV format and identify words that might be unfamiliar to a non-native English speaker who is familiar with an amount of most common English words, or vocabulary level. Keep in mind that a native English speaker has an average vocabulary of 15,000 words, but a non-native English speaker with IELTS test score of 6 has an average vocabulary of 5,000 words. Considering the given vocabulary level, return a list of the unfamiliar words in CSV format. If you really sure that the non-native English speaker knows all the words, return: "none".
-//   <Given words list>flood,pencil,skirt,stall,carve,ledge,ceiling,meadow,stance,shovel,cute,plank,next,pay,enough,best,flow,clever,shake,sham,rind,weasel,grouse,bury,shrivel,pastiche,discomfit,sully
-//   <Vocabulary level>4000
-//   <Unfamiliar words list>stall,ledge,ceiling,stance,sham,rind,weasel,grouse,shrivel,pastiche,discomfit
-//   <Given words list>flood,pencil,skirt,stall,carve,ledge,ceiling,meadow,stance,shovel,cute,plank,next,pay,enough,best,flow,clever,shake,sham,rind,weasel,grouse,bury,shrivel,pastiche,discomfit,sully
-//   <Vocabulary level>8000
-//   <Unfamiliar words list>rind,grouse,shrivel,pastiche,discomfit
-//   <Given words list>${text}
-//   <Vocabulary level>${vocabulary}
-//   <Unfamiliar words list>`;
-// }
-// function generatePrompt(text, vocabulary) {
-//   return `As an advanced AI language model, your task is to analyze the given list of words and identify those that may be unfamiliar to a non-native English speaker with a vocabulary level of ${vocabulary} words. For context, a native English speaker has an average vocabulary of 15,000 words, while a non-native English speaker with an IELTS test score of 6 typically knows around 5,000 words. Please return a list of the unfamiliar words in CSV format. If you believe that the non-native English speaker would be familiar with all the words given their vocabulary level, please return "none".
-
-//   Here are some examples to guide you:
-//   If the given words are 'flood,pencil,skirt,stall,carve,ledge,ceiling,meadow,stance,shovel,cute,plank,next,pay,enough,best,flow,clever,shake,sham,rind,weasel,grouse,bury,shrivel,pastiche,discomfit,sully' and the vocabulary level is 4000, the unfamiliar words might be 'stall,ledge,ceiling,stance,sham,rind,weasel,grouse,shrivel,pastiche,discomfit'.
-//   If the given words are the same and the vocabulary level is 8000, the unfamiliar words might be 'rind,grouse,shrivel,pastiche,discomfit'.
-
-//   Now, here is the list of words for you to analyze:
-//   ${text}
-
-//   Based on your expertise, please identify the unfamiliar words for a non-native English speaker with a vocabulary level of ${vocabulary} words. Remember to return the words in CSV format. If all words are likely to be familiar, please return "none".`;
-// }
 
 function generatePrompt(text, vocabulary) : ChatCompletionRequestMessage[]{
   return [
